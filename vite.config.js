7dcc7ab3626c9db9c034a0251a85812f96621c0d
MoisesCert <moisescert@outlook.com>
2022-12-16 08:39:54 +0100

    Added vite configuration for base path

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+// base hace que al hacer el build en las rutas relativas le pong por defecto ese texto al principio
+export default defineConfig({
+    base: '/js-vite-blackjack/',
+    plugins: []
+});
+
